812fe820e33b29fe95a8a8c9b378852642b8a3b4
Marcelo Oliveira <marbmo@gmail.com>
2019-05-27 22:19:40 -0300

    Tentativa de correção de erro Heroku

--- routes/upload.js
+++
@@ -1,8 +0,0 @@
-const { app, hbs, bodyParser, multer } = require('../config');
-const upload = multer({ dest: '../public/uploads/' });
-
-const uploadPicture = require('../controller/picture/create');
-
-app.post('/upload/:escolaId', upload.single('photo'), uploadPicture);
-
-module.exports = app;
